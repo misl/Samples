6d222c32c6f10fd17ac4ecdac1c21a159e0563f5
Minto van der Sluis <minto@xup.nl>
2014-01-08 18:43:42 +0100

    #10: Improved calculator service implementation
    - improved Quotient.toString() to support '5 R 1' notation
    - made it a real osgi service using blueprint

--- Calculator/calculator-simple/src/main/java/nl/xup/samples/calculator/simple/QuotientImpl.java
+++ Calculator/calculator-simple/src/main/java/nl/xup/samples/calculator/simple/QuotientImpl.java
@@ -55,6 +55,6 @@ public class QuotientImpl implements Quotient {
   
   @Override
   public String toString() {
-    return "quotient = " + quotient + "\nremainder = " + remainder + "\n";
+    return "" + quotient + " R " + remainder + "\n";
   }
 }
